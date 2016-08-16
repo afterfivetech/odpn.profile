546c65bf11f1d850b5f92ee2d9d304893b28f05a
glenn <glenn@glenn-Lenovo-G480>
2016-08-16 20:06:24 +0800

    custom browser view for members home folder

--- odpn/profile/browser/profile_view.py
+++ odpn/profile/browser/profile_view.py
@@ -1,6 +1,8 @@
 from five import grok
 from plone.directives import dexterity, form
 from odpn.profile.content.profile import IProfile
+from plone import api
+from Products.CMFCore.utils import getToolByName
 
 grok.templatedir('templates')
 
@@ -9,4 +11,29 @@ class Index(dexterity.DisplayForm):
     grok.require('zope2.View')
     grok.template('profile_view')
     grok.name('view')
+    
+    def user_info(self):
+        user_id = self.context.id
+        membership = getToolByName(self.context, 'portal_membership')
+        user = membership.getMemberById(user_id)
+        info = {'portrait':'#',
+                'fullname':'',
+                'industry':'',
+                'contact_details':'',
+                'primary_competencies':'',
+                'secondary_competencies':'',
+                'biography':''}
+        if user:
+            if user.getPersonalPortrait():
+                info['portrait'] = user.getPersonalPortrait().absolute_url()
+            info['fullname'] = user.getProperty('first_name')+' '+user.getProperty('mid_initial')+'. '+user.getProperty('last_name')
+            info['industry'] = user.getProperty('industry')
+            info['primary_competencies'] = user.getProperty('primary_competencies')
+            info['secondary_competencies'] = user.getProperty('secondary_competencies')
+            info['biography'] = user.getProperty('description')
+            if user.getProperty('telephone_no'):
+                info['contact_details'] = 'Tel. No. '+user.getProperty('telephone_no')+' '
+            if user.getProperty('cellphone_no'):
+                info['contact_details'] += 'Cell. No. '+user.getProperty('cellphone_no')
+        return info
 
