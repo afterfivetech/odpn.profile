8872fefaa493d66e239f8e5266c9b437a9f923c4
glenn <glenn@glenn-Lenovo-G480>
2016-08-22 16:39:23 +0800

    disable parent portlet

--- odpn/profile/content/profile.py
+++ odpn/profile/content/profile.py
@@ -23,6 +23,15 @@ from collective import dexteritytextindexer
 
 from odpn.profile import MessageFactory as _
 
+from zope.lifecycleevent.interfaces import IObjectAddedEvent
+from zope.component import getMultiAdapter, getUtility
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.constants import USER_CATEGORY
+from plone.portlets.constants import GROUP_CATEGORY
+from plone.portlets.constants import CONTENT_TYPE_CATEGORY
+from plone.portlets.constants import CONTEXT_CATEGORY
+
 
 # Interface class; used to define content-type schema.
 
@@ -33,3 +42,11 @@ class IProfile(form.Schema, IImageScaleTraversable):
     pass
 
 alsoProvides(IProfile, IFormFieldProvider)
+
+@grok.subscribe(IProfile, IObjectAddedEvent)
+def _createObject(context, event):
+    for manager_name in ('plone.leftcolumn','plone.rightcolumn'):
+        manager = getUtility(IPortletManager, name=manager_name)
+        assignable = getMultiAdapter((context, manager,), ILocalPortletAssignmentManager)
+        for category in (GROUP_CATEGORY,CONTEXT_CATEGORY,USER_CATEGORY):
+            assignable.setBlacklistStatus(category, 1)
