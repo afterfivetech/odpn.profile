e011f189fce88e600e97ad21278954cf4753029a
glenn <glenn@glenn-Lenovo-G480>
2016-08-23 11:27:42 +0800

    add table of competency types on controlpanel

--- odpn/profile/browser/odpn_settings.py
+++ odpn/profile/browser/odpn_settings.py
@@ -1,8 +1,18 @@
 from five import grok
 from Products.CMFCore.interfaces import ISiteRoot
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from odpn.profile.interfaces import ICompetencyDGForm
 
 grok.templatedir('templates')
 
 class odpn_settings(grok.View):
     grok.name('odpn-settings')
     grok.context(ISiteRoot)
+    
+    def list_competencies(self):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ICompetencyDGForm, check=False)
+        if settings.competencies:
+            return [val['primary_competencies_values'] for val in settings.competencies]
+        return []
